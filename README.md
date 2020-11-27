787681ad17d9bbbd62c30cd50bcf0c1b88c2099c
Kevin Brubeck Unhammer <unhammer@fsfe.org>
2020-11-27 11:21:51 +0100

    Make it a minor mode that runs a timer (:eval is laggy)
    + include ts-doy fix and suggestions from @alphapapa
      https://www.reddit.com/r/emacs/comments/k1e4eu/show_next_orgappointment_in_modeline/gdprvzf/
    
    + defcustoms
    
    + mouse click

---
+++ README.md
@@ -0,0 +1,7 @@
+```emacs-lisp
+(use-package org-upcoming-modeline
+  :after org
+  :load-path "~/src/org-upcoming-modeline"
+  :config
+  (org-upcoming-modeline-mode))
+```
--- org-upcoming-modeline.el
+++ org-upcoming-modeline.el
@@ -1,10 +1,38 @@
+;;; org-upcoming-modeline.el --- Show next org event in mode line -*- lexical-binding: t -*-
+
+;; Copyright (C) 2020 Kevin Brubeck Unhammer
+
+;; Author: Kevin Brubeck Unhammer <unhammer@fsfe.org>
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.1") (ts "0.2") (org-ql "0.5"))
+;; URL: https://github.com/unhammer/org-upcoming-modeline
+;; Keywords: convenience, calendar
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;; Just M-x org-upcoming-modeline-mode to turn it on.
+
 ;;; Code:
 
-(require 'dash)
 (require 'ts)
 (require 'org-ql)
 
-(defsubst org-upcoming-modeline-parse-ts (org-ts-string)
+(defsubst org-upcoming-modeline--parse-ts (org-ts-string)
   "Get ts object for Org timestamp string ORG-TS-STRING but only if it has hour/minute part."
   (pcase-let* ((`(,second ,minute ,hour ,day ,month ,year)
                 (save-match-data
@@ -13,52 +41,116 @@
          hour
          (make-ts :second 0 :minute minute :hour hour :day day :month month :year year))))
 
-(defvar org-upcoming-modeline-duration-threshold 3600
-  "If less than this many seconds, print as duration instead of timestamp.")
+(defgroup org-upcoming-modeline nil
+  "Options for showing upcoming org event in the mode line."
+  :tag "Org Upcoming Modeline"
+  :group 'org)
 
-(defun org-upcoming-modeline ()
-  "Make a modeline string of the first upcoming org timestamp."
-  (when-let* ((now (ts-now))
-              (tomorrow (ts-format "%Y-%m-%d" (ts-adjust 'day 1 now)))
-              (tasks (org-ql-select (org-agenda-files)
-                       `(ts-active :from today :to ,tomorrow)
-                       :action (lambda ()
-                                 (-let* (((&alist "ITEM" headline
-		                                  "TIMESTAMP" timestamp
-		                                  "DEADLINE" deadline
-		                                  "SCHEDULED" scheduled)
-	                                  (org-entry-properties)))
-                                   (seq-uniq (cl-loop
-                                              for org-ts-string in (list timestamp deadline scheduled)
-                                              when org-ts-string
-                                              for time = (org-upcoming-modeline-parse-ts org-ts-string)
-                                              when (and time
-                                                        (ts> time now))
-                                              collect
-                                              (cons time headline)))))))
-              (sorted-tasks (seq-sort-by #'car #'ts< (apply 'append tasks)))
-              (task (car sorted-tasks))
-              (event-time (car task))
-              (event-title (cdr task))
-              (seconds-until (ts-difference event-time now))
-              (days-until (- (ts-day event-time)
-                             (ts-day now)))
-              (event-time-string (cond ((<= seconds-until org-upcoming-modeline-duration-threshold)
-                                        (ts-human-format-duration seconds-until 'abbreviate))
-                                       ((= 0 days-until)
-                                        (ts-format "%H:%M" event-time))
-                                       ((= 1 days-until)
-                                        (ts-format "tomorrow %H:%M" event-time))
-                                       (t ; > 1 days-until
-                                        (ts-format "%a %H:%M" event-time)))) )
-    (propertize
-     (format " ⏰ %s: %s" event-time-string event-title)
-     'face 'org-level-4
-     'help-echo (format "%s at <%s>"
-                        event-title
-                        (ts-format "%Y-%m-%d %H:%M" event-time)))))
+(defcustom org-upcoming-modeline-duration-threshold 3600
+  "If less than this many seconds, print as duration instead of timestamp."
+  :group 'org-upcoming-modeline
+  :type 'integer)
 
-(add-to-list 'global-mode-string '(:eval (org-upcoming-modeline)) 'append)
+(defcustom org-upcoming-modeline-l10n '((tomorrow . "tomorrow"))
+  "Localisation."
+  :group 'org-upcoming-modeline
+  :type '(alist :value-type string))
+
+(defcustom org-upcoming-modeline-on-idle-seconds 5
+  "Minimum seconds of idle-time before updating upcoming event in the mode line."
+  :group 'org-upcoming-modeline
+  :type 'integer)
+
+
+
+(defvar org-upcoming-modeline-string nil
+  "Value from last `org-upcoming-modeline--recompute'.")
+;;;###autoload(put 'org-upcoming-modeline-string 'risky-local-variable t)
+(put 'org-upcoming-modeline-string 'risky-local-variable t)
+
+(defvar org-upcoming-modeline--timer nil)
+
+(defun org-upcoming-modeline--recompute ()
+  "Make a mode line string of the first upcoming org timestamp.
+Store it along in `org-upcoming-modeline-string'."
+  (pcase-let* ((now (ts-now))
+               (items (remove
+                       nil
+                       (org-ql-select (org-agenda-files)
+                         '(ts-active :from 0 :to 1)
+                         :action '(when-let* ((mark (point-marker))
+                                              (bound (save-excursion (outline-next-heading) (point)))
+                                              (time (save-excursion
+                                                      (car (sort (cl-loop
+                                                                  while (re-search-forward org-tsr-regexp bound 'noerror)
+                                                                  for org-ts-string = (match-string 1)
+                                                                  when org-ts-string
+                                                                  for time = (org-upcoming-modeline--parse-ts org-ts-string)
+                                                                  when time collect time)
+                                                                 #'ts<)))))
+                                    (cons time mark)))))
+               (`(,time . ,marker) (car (seq-sort-by #'car #'ts< items)))
+               (heading (org-with-point-at marker
+                          (org-link-display-format (nth 4 (org-heading-components)))))
+               (seconds-until (ts-difference time now))
+               ;; NOTE: Using day of year to avoid end-of-month turnover in day number.
+               (days-until (- (ts-doy time) (ts-doy now)))
+               (time-string (cond ((<= seconds-until org-upcoming-modeline-duration-threshold)
+                                   (ts-human-format-duration seconds-until 'abbreviate))
+                                  ((= 0 days-until)
+                                   (ts-format "%H:%M" time))
+                                  ((= 1 days-until)
+                                   (concat (cdr (assoc 'tomorrow org-upcoming-modeline-l10n))
+                                           (ts-format " %H:%M" time)))
+                                  (t      ; > 1 days-until
+                                   (ts-format "%a %H:%M" time)))))
+    (setq org-upcoming-modeline-string
+          (propertize (format " ⏰ %s: %s" time-string heading)
+                      'face 'org-level-4
+                      'help-echo (format "%s left until %s"
+                                         (ts-human-format-duration seconds-until)
+                                         heading)
+                      'org-upcoming-marker marker
+                      'mouse-face 'mode-line-highlight
+                      'local-map org-upcoming-modeline-map))))
+
+;;;###autoload
+(define-minor-mode org-upcoming-modeline-mode
+  "Show next upcoming org-mode event in mode line"
+  :group 'org-upcoming-modeline
+  :global t
+  (if org-upcoming-modeline-mode
+      (progn
+        (add-to-list 'global-mode-string 'org-upcoming-modeline-string 'append)
+        (setq org-upcoming-modeline--timer (run-with-idle-timer
+                                            org-upcoming-modeline-on-idle-seconds
+                                            'repeat
+                                            #'org-upcoming-modeline--recompute))
+        ;; Also compute immediately on first starting the mode, for that first-run feel:
+        (org-upcoming-modeline--recompute))
+    ;; Turn off:
+    (delq 'org-upcoming-modeline-string global-mode-string)
+    (when (timerp org-upcoming-modeline--timer)
+      (cancel-timer org-upcoming-modeline--timer))))
+
+(defun org-upcoming-modeline-goto (event)
+  "Show upcoming org EVENT."
+  (interactive "e")
+  (setq foo-event event)
+  (when-let* ((text (car (cl-fifth (cadr event)))) ; TODO there's gotta be some event api for this
+              (marker (get-text-property 0 'org-upcoming-marker text)))
+    (switch-to-buffer (marker-buffer marker))
+    (widen)
+    (goto-char (marker-position marker))
+    (org-show-entry)
+    (org-back-to-heading t)
+    (org-cycle-hide-drawers 'children)
+    (org-reveal)))
+
+(defconst org-upcoming-modeline-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [mode-line down-mouse-1] 'org-upcoming-modeline-goto)
+    map))
 
 (provide 'org-upcoming-modeline)
 
